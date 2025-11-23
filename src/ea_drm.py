8d1f3c39f1a434ed5403bcf173d90dbdbaa18df3
Matty <54020396+matty-ross@users.noreply.github.com>
2025-11-23 20:19:53 +0100

    Refactor and bugfixes.

--- src/ea_drm.py
+++ src/ea_drm.py
@@ -10,9 +10,8 @@ from Crypto.Util import Padding
 
 LICENSE_KEY = base64.b64decode("QTJyLdCC77DcZFfFdmjKCQ==")
 
-# TODO: move these constants to a config file (or try to compute the offsets?)
-KEY = bytes(16)
-ENCRYPTED_SECTIONS_TABLE_OFFSET = 0x0
+# TODO: move these offsets to a config file (or try to compute them?)
+ENCRYPTED_SECTIONS_TABLE_OFFSET = 0x3EE
 
 
 @dataclass
@@ -27,28 +26,26 @@ class EA_DRM:
 
     def __init__(self, license_path: str, pe_path: str):
         self._license_path = pathlib.Path(license_path)
-        self._decryption_key = bytes()
         self._pe_path = pathlib.Path(pe_path)
+        self._decryption_key = self._get_decryption_key()
         self._pe = pefile.PE(pe_path, fast_load=True)
+        self._ooa_rva = self._get_oaa_section_rva()
 
 
-    def retrieve_decryption_key(self) -> None:
-        with open(self._license_path, 'rb') as license_file:
-            license_file.seek(0x41)
-            encrypted_license = license_file.read()        
-        decrypted_license = EA_DRM._aes_decrypt(LICENSE_KEY, encrypted_license).decode('utf-8')
-        # TODO: update decryption key
+    def save(self) -> None:
+        new_pe_path = self._pe_path.with_stem(self._pe_path.stem + '_no_drm')
+        self._pe.write(new_pe_path)
 
     
-    def decrypt_sections(self):
-        count = self._read_pe(ENCRYPTED_SECTIONS_TABLE_OFFSET, 'B')
+    def decrypt_sections(self) -> None:
+        count = self._read_ooa(ENCRYPTED_SECTIONS_TABLE_OFFSET, 'B')
         for i in range(count):
             offset = ENCRYPTED_SECTIONS_TABLE_OFFSET + 0x1 + i * 0x30
             encrypted_section = EncryptedSection(
-                rva=self._read_pe(offset + 0x0, '<L'),
-                encrypted_size=self._read_pe(offset + 0x4, '<L'),
-                decrypted_size=self._read_pe(offset + 0x8, '<L'),
-                padding=self._read_pe(offset + 0x20, '16B'),
+                rva=self._read_ooa(offset + 0x0, '<L'),
+                encrypted_size=self._read_ooa(offset + 0x4, '<L'),
+                decrypted_size=self._read_ooa(offset + 0x8, '<L'),
+                padding=self._read_ooa(offset + 0x20, '16B'),
             )
             self._decrypt_section(encrypted_section)
 
@@ -62,13 +59,31 @@ class EA_DRM:
         
         padding = bytearray(16)
         for i in range(16):
-            padding[i] = (decrypted_data[encrypted_section.decrypted_size - 0x10 + i] + 0x14) ^ encrypted_section.padding[i]
+            padding[i] = decrypted_data[encrypted_section.decrypted_size - 0x10 + i] ^ encrypted_section.padding[i]
 
         self._pe.set_bytes_at_rva(encrypted_section.rva, decrypted_data + padding)
 
+
+    def _get_decryption_key(self) -> bytes:
+        with open(self._license_path, 'rb') as license_file:
+            license_file.seek(0x41)
+            encrypted_license = license_file.read()        
+        decrypted_license = EA_DRM._aes_decrypt(LICENSE_KEY, encrypted_license).decode('utf-8')
+        
+        start = decrypted_license.find('<CipherKey>') + 11
+        end = decrypted_license.find('</CipherKey>')
+        return base64.b64decode(decrypted_license[start:end])[:16]
     
-    def _read_pe(self, offset: int, format: str):
-        rva = self._pe.get_rva_from_offset(offset)
+
+    def _get_oaa_section_rva(self) -> int:
+        for section in self._pe.sections:
+            if section.Name.strip(b'\x00') == b'.ooa':
+                return section.VirtualAddress
+        raise Exception(f"No .ooa section in the PE file.")
+
+    
+    def _read_ooa(self, offset: int, format: str):
+        rva = self._ooa_rva + offset
         buffer = self._pe.get_data(rva, struct.calcsize(format))
         value = struct.unpack(format, buffer)
         return value[0] if len(value) == 1 else value
--- src/main.py
+++ src/main.py
@@ -4,9 +4,12 @@ from ea_drm import EA_DRM
 
 
 def main() -> None:
+    license_path = filedialog.askopenfilename()
     pe_path = filedialog.askopenfilename()
-    ea_drm = EA_DRM(pe_path)
+    
+    ea_drm = EA_DRM(license_path, pe_path)
     ea_drm.decrypt_sections()
+    ea_drm.save()
 
 
 if __name__ == '__main__':
